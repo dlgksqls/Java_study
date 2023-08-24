e115b3f7af5642c1fcb2a8dfb31d6c7a9d3bc14d
LeeHB <horainlee87@gmail.com>
2023-08-24 16:43:35 +0900

    ãstudying to array

--- src/Chap_05/_03_MultiArray.java
+++ src/Chap_05/_03_MultiArray.java
@@ -3,14 +3,14 @@ package Chap_05;
 public class _03_MultiArray {
     public static void main(String[] args) {
         String[][] seats = new String[][] {
-                {"A1","A2","A3","A4","A5"},
+                {"A1","A2","A3","A4","A5","A6"},
                 {"B1","B2","B3","B4","B5"},
                 {"C1","C2","C3","C4","C5"},
         };
         System.out.println(seats[1][2]); // B3
 
         String[][] seats_2 = {
-                {"A1","A2","A3"},
+                {"A1","A2","A3",},
                 {"B1","B2","B3","B4"},
                 {"C1","C2","C3","C4","C5"}
         };
